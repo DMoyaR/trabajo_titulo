5400ed9c99296c4300d54c943ae4de30dae0d4f0
DmoyaR <dmoyar@utem.cl>
2025-10-02 09:50:54 -0300

    Se habilita botón para eliminar temas

--- backend/corsheaders/middleware.py
+++ backend/corsheaders/middleware.py
@@ -5,7 +5,7 @@ class CorsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.allow_origin = "*"
-        self.allow_methods = "GET, POST, OPTIONS"
+        self.allow_methods = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
         self.allow_headers = "Content-Type, Authorization"
         self.allow_credentials = "true"
 
