1cac5a4b7ed5eda304ff17c11e2703e59a4faabc
DmoyaR <dmoyar@utem.cl>
2025-10-02 03:47:11 -0300

    Se agrega subirarchivo sin popup

--- frontend/src/app/features/docente/shareduploaddialog/upload-dialog.component.ts
+++ frontend/src/app/features/docente/shareduploaddialog/upload-dialog.component.ts
@@ -1,28 +1,15 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // 👈
+import { SubirArchivoComponent, DatosPublicacion } from '../subirarchivo/subirarchivo.component';
 
 @Component({
   selector: 'docente-upload-dialog',
   standalone: true,
-  imports: [CommonModule, FormsModule], // 👈
+  imports: [CommonModule, SubirArchivoComponent],
   templateUrl: './upload-dialog.component.html',
   styleUrls: ['./upload-dialog.component.css'],
 })
 export class UploadDialogComponent {
   @Output() close = new EventEmitter<void>();
-  @Output() confirm = new EventEmitter<{ fecha: string; archivo: string; comentario: string }>();
-
-  fecha: string = '';
-  archivo: string = '';
-  comentario: string = '';
-
-  onFile(e: Event) {
-    const input = e.target as HTMLInputElement;
-    this.archivo = (input.files && input.files[0]) ? input.files[0].name : '';
-  }
-
-  doConfirm() {
-    this.confirm.emit({ fecha: this.fecha, archivo: this.archivo, comentario: this.comentario });
-  }
+  @Output() confirm = new EventEmitter<DatosPublicacion>();
 }
