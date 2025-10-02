eb7927f3aea90c12415f8b8e27d55acf5551d0d6
DmoyaR <dmoyar@utem.cl>
2025-10-02 08:55:44 -0300

    Se agregan modelos no funcionales

--- backend/corsheaders/middleware.py
+++ backend/corsheaders/middleware.py
@@ -1,8 +1,28 @@
+from django.http import HttpResponse
+
+
 class CorsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.allow_origin = "*"
+        self.allow_methods = "GET, POST, OPTIONS"
+        self.allow_headers = "Content-Type, Authorization"
+        self.allow_credentials = "true"
+
+    def _add_cors_headers(self, response):
+        response.setdefault("Access-Control-Allow-Origin", self.allow_origin)
+        response.setdefault("Access-Control-Allow-Methods", self.allow_methods)
+        response.setdefault("Access-Control-Allow-Headers", self.allow_headers)
+        if self.allow_credentials:
+            response.setdefault(
+                "Access-Control-Allow-Credentials", self.allow_credentials
+            )
+        return response
 
     def __call__(self, request):
-        response = self.get_response(request)
-        response.setdefault('Access-Control-Allow-Origin', '*')
-        return response
+        if request.method == "OPTIONS":
+            response = HttpResponse(status=200)
+        else:
+            response = self.get_response(request)
+
+        return self._add_cors_headers(response)
